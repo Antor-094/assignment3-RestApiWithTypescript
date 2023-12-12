fa089de40716c0850f3cdc7423149cdd1bbac099
Antor-094 <mdantor556677889@gmail.com>
2023-12-12 11:32:58 +0600

    Course Created successfully

---
+++ src/app/modules/Course/course.route.ts
@@ -0,0 +1,11 @@
+
+import express from 'express'
+import validateRequest from '../../middlewares/validateRequest'
+import { CourseValidations } from './course.validation'
+import { courseControllers } from './course.controller'
+
+const router = express.Router()
+
+router.post('/',validateRequest(CourseValidations.TCourseValidationSchema),courseControllers.createCourse)
+
+export const CourseRoute = router
